31ad1168b8a959fbd0af1cd9ea7e707fa40f9f03
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-03 17:32:57 +0400

    Update split.py

--- split.py
+++ split.py
@@ -1,21 +1,18 @@
 def split(str1,separator,maxsplit):
-    i = 0;
-    j = 0;
-    k = 0;
+    i = j = k = 0
     list1 = []
     while k < maxsplit:
-        while (str1[j] != separator):
+        while (str1[j] != separator) and (j < len(str1)-1):
             j += 1
-            if(j == len(str1)):
-                break;
-        str2 = str1[i:j]
+        if(j == len(str1)-1):
+            str2 = str1[i:]
+        else:
+            str2 = str1[i:j]
         list1.append(str2)
-        print(list1) 
-        j = j +1 
+        print(list1)
+        j += 1
         i = j
-       
-        
         k += 1
-    
-split("hhhh*aaa*aaa  ","*",1) ## problem during the last string appending
+        print(i,"  ",j )
 
+split("h*a*a   "," ",2)
