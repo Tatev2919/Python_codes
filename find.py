e54dedac8d5455aea867ba05895481c72d25c69a
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-04 21:41:28 +0400

    Update find.py

--- find.py
+++ find.py
@@ -1,27 +1,22 @@
-
 fullstr = "jashajsaj"
 
-def func(str,start,end):
+def func(str,start=0,end =len(fullstr)):
     h = 0
     flag = False
     if(len(str)>len(fullstr)):
-      #  print(False)
-        return False
-    if (len(str) < start) | (len(str) > end):
-        print(False)
-        return False
+        return -1
+    if (start<0) | (end > len(fullstr)):
+        return -1
     for i in range(start,end):
         flag = True
         if fullstr[i] == str[h]:
             h = h + 1
             flag = False
         if h == len(str):
-            print(i-h+1)
             return i-h+1;
-        elif flag: 
+        elif flag:
             h = 0;
     if h < len(str):
-        print(-1," ",h)
         return -1
-    
-func("js",0,16);
+
+print(func("js",0,8));
