67b1d8caf0425810ae0af5180665fad8a3f47c66
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-04 17:35:39 +0400

    Update replace.py

--- replace.py
+++ replace.py
@@ -2,21 +2,20 @@ def replace(str1,oldval,newval,count):
     k = len(oldval)
     replacedStr = ""
     count1 = count
-    
     for j in range(len(str1)-k +1):
         for i in range(j,len(str1)-k+1):
             if(str1[j:j+k] == oldval):
-                if(count1 == count): 
-                    replacedStr = str1[:j] + newval + str1[j+k:];
-                    print(replacedStr)
-                    count1 -=1
-                    print(j)
+                count1-=1;
+                if(count1 == count - 1):
+                    str1 = str1[:j] + newval + str1[j+k:];
+                    print(str1)
+                    print(count1)
                     break
                 else:
-                    replacedStr = replacedStr[:j-2] + newval + str1[j+k:];
-        if(count == 0):
+                    str1 = str1[:j-2*(count1)] + newval + str1[j+k:];
+        if (count1 == -1):
             break
     return replacedStr
-    
-    
-print(replace("This is a Stringand String 111","String","char",3))
+
+
+replace("This is a Stringand String 11","String","char",2)
