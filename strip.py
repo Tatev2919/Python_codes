449c44c6324de2987a10e67c624d91dbe88cf70c
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-04 23:36:36 +0400

    Update strip.py
    The strip() method removes any leading (spaces at the beginning) and trailing (spaces at the end) characters (space is the default leading character to remove)

--- strip.py
+++ strip.py
@@ -1,16 +1,28 @@
-def strip(str1):
-    i = 0
-    str2 = ""
-    str3 = ""
-    while str1[i]==" ":
-        i += 1
-        continue
-    str2 = str1[i:]
-    j = len(str2)-1
-    while str2[j] == " ":
-        j -=1
-        continue
-    str3 = str2[:j+1];
+def strip(str1,separator=" "):
+    h = 0
+    str2 = str3 = ""
+    flag = False
+    for i in range(len(str1)):
+        flag = False
+        for j in range(len(separator)):
+            if(str1[i] == separator[j]):
+                flag = True
+                h += 1
+                break
+        if(flag == False):
+            break
+    str2 = str1[h:]
+    m = len(str2)
+    for i in range(len(str2)-1,0,-1):
+        flag = False
+        for j in range(len(separator)):
+            if(str2[i] == separator[j]):
+                flag = True
+                m -=1
+                break
+        if(flag == False):
+            break
+    str3 = str2[:m]
     return str3
 
-print(strip("           ahh     hbc "))
+print(strip("    **//?*%%/*^/*/ahh     hbc***/*/*/*^^^ "," */?%^"))
